10afa780ce6483fc6d8fb9f96703840baab98f26
AnatoliiGady <grek-prost@yandex.ru>
2022-12-29 21:45:09 +0900

    2. Каркас шахматной доски

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,23 +1,31 @@
 package ru.job4j.chess;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@Disabled("Тесты отключены. Удалить аннотацию после реализации всех методов по заданию.")
 public class LogicTest {
 
-    @Test
-    public void whenMoveThenFigureNotFoundException()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    @Test(expected = FigureNotFoundException.class)
+    public void whenNotFound() throws OccupiedCellException, FigureNotFoundException, ImpossibleMoveException {
+        Logic test = new Logic();
+        test.add(new BishopBlack(Cell.C1));
+        test.move(Cell.A4, Cell.C8);
+    }
+
+    @Test(expected = OccupiedCellException.class)
+    public void whenOccupiedCellException() throws OccupiedCellException, FigureNotFoundException, ImpossibleMoveException {
         Logic logic = new Logic();
-        FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () -> {
-            logic.move(Cell.C1, Cell.H6);
-        });
-        assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
+        logic.add(new BishopBlack(Cell.C1));
+        logic.add(new BishopBlack(Cell.D2));
+        logic.move(Cell.C1, Cell.E3);
+    }
+
+    @Test(expected = ImpossibleMoveException.class)
+    public void whenImpossibleMove() throws OccupiedCellException, FigureNotFoundException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+        logic.move(Cell.C1, Cell.A1);
     }
 }
---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.chess.firuges.black;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import ru.job4j.chess.firuges.Cell;
+
+import javax.swing.text.Position;
+
+
+public class BishopBlackTest {
+
+    @Test
+    void whenSameCell() {
+        BishopBlack testMove = new BishopBlack(Cell.A1);
+        assertThat(testMove.position()).isEqualTo(Cell.A1);
+    }
+
+    @Test
+    void whenCorrectPosition() {
+        BishopBlack testMove = new BishopBlack(Cell.A2);
+        testMove.copy(Cell.A2);
+        assertThat(testMove.position()).isEqualTo(Cell.A2);
+    }
+
+    @Test
+    void whenWayCorrectPosition() {
+        BishopBlack testMove = new BishopBlack(Cell.C1);
+        Cell[] steps = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        assertThat(steps).isEqualTo(testMove.way(Cell.G5));
+    }
+
+}
