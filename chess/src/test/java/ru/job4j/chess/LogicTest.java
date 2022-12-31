bf6d4a0a308289443c20d12c1a9e4b7b1d155e39
AnatoliiGady <grek-prost@yandex.ru>
2022-12-31 10:47:13 +0900

    2. Каркас шахматной доски

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,33 +1,37 @@
 package ru.job4j.chess;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
 
 public class LogicTest {
 
-    @Test(expected = FigureNotFoundException.class)
-    public void whenNotFound()
-            throws OccupiedCellException, FigureNotFoundException, ImpossibleMoveException {
-        Logic test = new Logic();
-        test.add(new BishopBlack(Cell.C1));
-        test.move(Cell.A4, Cell.C8);
+    @Test
+    public void whenNotFound() {
+        FigureNotFoundException thrown = Assertions.assertThrows(FigureNotFoundException.class, () -> {
+            Logic test = new Logic();
+            test.add(new BishopBlack(Cell.C1));
+            test.move(Cell.A4, Cell.C8);
+        });
     }
 
-    @Test(expected = OccupiedCellException.class)
-    public void whenOccupiedCellException()
-            throws OccupiedCellException, FigureNotFoundException, ImpossibleMoveException {
-        Logic logic = new Logic();
-        logic.add(new BishopBlack(Cell.C1));
-        logic.add(new BishopBlack(Cell.D2));
-        logic.move(Cell.C1, Cell.E3);
+    @Test
+    public void whenOccupiedCellException() {
+        OccupiedCellException thrown = Assertions.assertThrows(OccupiedCellException.class, () -> {
+            Logic logic = new Logic();
+            logic.add(new BishopBlack(Cell.C1));
+            logic.add(new BishopBlack(Cell.D2));
+            logic.move(Cell.C1, Cell.E3);
+        });
     }
 
-    @Test(expected = ImpossibleMoveException.class)
-    public void whenImpossibleMove()
-            throws OccupiedCellException, FigureNotFoundException, ImpossibleMoveException {
-        Logic logic = new Logic();
-        logic.add(new BishopBlack(Cell.C1));
-        logic.move(Cell.C1, Cell.A1);
+    @Test
+    public void whenImpossibleMove() {
+        ImpossibleMoveException thrown = Assertions.assertThrows(ImpossibleMoveException.class, () -> {
+            Logic logic = new Logic();
+            logic.add(new BishopBlack(Cell.C1));
+            logic.move(Cell.C1, Cell.A1);
+        });
     }
 }
